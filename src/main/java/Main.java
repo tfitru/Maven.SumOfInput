e7088ce544d585cc9d7a93eca6cdfef710686e08
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-06-28 22:14:06 -0400

    Sum of the user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
 
+        int userInput = input.nextInt();
+        int sum = 0;
+
+        for(int i = 0; i<userInput; i++) {
+            sum += i + userInput;
+        }
+        System.out.println(sum);
     }
 }
